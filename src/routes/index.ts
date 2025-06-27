09aaed088bcc03779b821fdbe6eb3b25d2b25197
snehasishcodes <snehasishcodes@gmail.com>
2025-06-27 14:10:25 +0530

    include cronjob route for server cleanup every X minutes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,10 @@ import root from "./root";
 import form from "./form";
 import get from "./get";
 import upload from "./upload";
+import cronjob from "./cronjob";
 
 const router = express.Router();
 
-router.use("/", root, form, get, upload);
+router.use("/", root, form, get, upload, cronjob);
 
 export default router;
